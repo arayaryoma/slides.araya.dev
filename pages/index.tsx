980e180a9ad0c606feb5b1ea8768e20cd9ead9b2
arayaryoma <r1o.ryoma.abe@gmail.com>
2020-12-30 05:10:20 +0900

    Implement displaying list of slides from speakerdeck

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
+import { GetStaticProps } from "next";
+import { fetchDecks } from "@arayaryoma/speakerdeck-scraper";
+
 import React, { FC } from "react";
 
-const Home: FC = () => {
-  return <p></p>;
+interface Slide {
+  title?: string;
+  image?: string;
+}
+
+interface Props {
+  slides: Array<Slide>;
+}
+
+const Home: FC<Props> = (props) => {
+  return (
+    <div>
+      <ul>
+        {props.slides.map((slide, i) => (
+          <li key={i}>{slide.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const decks = await fetchDecks({ username: "arayaryoma" });
+
+  return {
+    props: {
+      slides: decks.map((deck) => ({
+        title: deck.title,
+        image: deck.previewImageSrc,
+      })),
+    },
+  };
 };
 
 export default Home;
